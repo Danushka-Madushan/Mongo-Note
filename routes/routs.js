9ffbd64d05d2cbff4d78d18d2f8fa1db4b18ac75
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-02-20 22:13:27 +0530

    code refactoring

--- routes/routs.js
+++ routes/routs.js
@@ -1,32 +1,6 @@
-const { MongoClient } = require('mongodb')
 const express = require('express')
 const router = express.Router()
-
-const authURI = "mongodb+srv://env0ytuserdata:DPDMadushan123.@yt0cluster.la24r61.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(authURI);
-
-const db = client.db("mongo-note");
-const notes = db.collection("notes");
-
-const InsertItem = async (id, data) => {
-    const result = await notes.insertOne({_id: id, content: `${data}`});
-    console.log(`A document was inserted with the _id: ${result.insertedId}`);
-}
-
-const RemoveItem = async (id) => {
-    const result = await notes.deleteOne({_id: id});
-
-    if (result.deletedCount === 1) {
-        console.log("Successfully deleted one document.");
-    } else {
-        console.log("No documents matched the query. Deleted 0 documents.");
-    }
-}
-
-const RequestNotes = async () => {
-    const cursor = notes.find({});
-    return await cursor.toArray();
-}
+const { InsertItem, RemoveItem, RequestNotes } = require('../scripts/mongo')
 
 router.get('/insert', (req, res) => {
     InsertItem('7d8328d8-680b-4782-8850-551a81b876f8', 'Alex is My Friend').catch(console.dir);
