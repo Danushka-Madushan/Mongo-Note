87cdc37fa576f453d68525448423c7cea2fbeea6
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-02-20 22:13:08 +0530

    mongodb client moved to another file

---
+++ scripts/mongo.js
@@ -0,0 +1,30 @@
+const { MongoClient } = require('mongodb')
+
+const authURI = "mongodb+srv://env0ytuserdata:DPDMadushan123.@yt0cluster.la24r61.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(authURI);
+
+const db = client.db("mongo-note");
+const notes = db.collection("notes");
+
+
+const InsertItem = async (id, data) => {
+    const result = await notes.insertOne({_id: id, content: `${data}`});
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+}
+
+const RemoveItem = async (id) => {
+    const result = await notes.deleteOne({_id: id});
+
+    if (result.deletedCount === 1) {
+        console.log("Successfully deleted one document.");
+    } else {
+        console.log("No documents matched the query. Deleted 0 documents.");
+    }
+}
+
+const RequestNotes = async () => {
+    const cursor = notes.find({});
+    return await cursor.toArray();
+}
+
+module.exports = {InsertItem, RemoveItem, RequestNotes}
