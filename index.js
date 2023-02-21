16d7497116619e7f8d48336f5e16dc8d1c81a2a1
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-02-21 19:07:57 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
 
+const dbRoutes = require('./routes/routs')
+
 app.use(express.static('public'))
+app.use('/db', dbRoutes)
 
 app.get('/', (req, res) => {
     res.status(200).render('index.html')
 })
-
-const dbRoutes = require('./routes/routs')
-app.use('/db', dbRoutes)
 
 app.listen(5000) // localhost:5000
