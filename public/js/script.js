81e75c11d3cf7877164f621f18dc960de5c1792f
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-02-21 19:23:50 +0530

    code Refactoring

--- public/js/script.js
+++ public/js/script.js
@@ -7,78 +7,31 @@ const NewNote = (content, id, refresh) => {
     $(ghostCalss).fadeOut(() => {
         let newItem = createNote(content, id, refresh)
         if (newItem) {
-            $(newItem).hide();
+            $(newItem).hide()
             userNotes.append(newItem)
-            $(newItem).fadeIn();
+            $(newItem).fadeIn()
         }
-    }); 
+    })
 }
 
-$('.addItem').click(() => {
-    let value = getInput()
-    if (value) {
-        NewNote(value)  
-    }
-});
-
-$('.items').on('click', '.remove', (e) => {
-    let target = e.currentTarget
-    let item = target.nextElementSibling.parentNode
-    if (target.childNodes.length <= 1) {
-        removeItemfromDB(item.id)
-        $(target.children[0]).fadeOut(100, () => {
-
-            let tick = document.createElement('i')
-            tick.className = 'fa-solid fa-circle-check'
-            $(target).css('color', '#009B77');
-            $(tick).hide();
-            target.append(tick)
-
-            $(tick).fadeIn(()=> {
-                $(item).slideUp();
-                $(item).fadeOut(() => {
-                    if (userNotes.children.length <= 2) {
-                        $(ghostCalss).fadeIn(() => {
-                            $(item).remove();
-                        });
-                    } else {
-                        $(item).remove();
-                    }
-                });
-            });
-        });
-    }
-});
-
-$('.items').on('click', '.copy', (e) => {
-    let content = e.currentTarget
-    .nextSibling
-    .parentNode
-    .children[1]
-    .children[0]
-    .innerText
-    navigator.clipboard.writeText(content)
-
-});
-
-$('.refresh').click( (e) => { 
-    let target = e.currentTarget
-    $(target).css('animation', 'setRotation 2s infinite linear');
+const RefreshNotes = () => {
+    let target = document.querySelector('.refresh')
+    $(target).css('animation', 'setRotation 2s infinite linear')
 
     const clearAnim = () => {
         $(target).fadeOut(()=>{
             $(target).css('animation', 'none');
             target.className = 'fa-solid fa-circle-check refresh'
             $(target).css('color', '#009B77');
-            $(target).fadeIn();
+            $(target).fadeIn()
             setTimeout(() => {
                 $(target).fadeOut(() => {
                     target.className = 'fa-solid fa-rotate refresh'
-                    $(target).css('color', '#fff');
-                    $(target).fadeIn();
+                    $(target).css('color', '#fff')
+                    $(target).fadeIn()
                 });
             }, 1000)
-        });
+        })
     }
 
     $.ajax({
@@ -94,7 +47,7 @@ $('.refresh').click( (e) => {
                     if (!idList.includes(each.id)) {
                         $(each).slideUp(() => {
                             each.remove()
-                        });
+                        })
                     }
                 }
                 for (let each of data) {
@@ -104,12 +57,67 @@ $('.refresh').click( (e) => {
                 for (let each of document.getElementsByClassName('note')) {
                     each.remove()
                 }
-                $(ghostCalss).fadeIn();
+                $(ghostCalss).fadeIn()
             }
             clearAnim()
         },
         error: (errMsg) => {
-            console.log(errMsg);
+            console.log(errMsg)
         }
-    });
-});
+    })
+}
+
+$('.addItem').click(() => {
+    let value = getInput()
+    if (value) {
+        NewNote(value)  
+    }
+})
+
+$('.items').on('click', '.remove', (e) => {
+    let target = e.currentTarget
+    let item = target.nextElementSibling.parentNode
+    if (target.childNodes.length <= 1) {
+        removeItemfromDB(item.id)
+        $(target.children[0]).fadeOut(100, () => {
+
+            let tick = document.createElement('i')
+            tick.className = 'fa-solid fa-circle-check'
+            $(target).css('color', '#009B77')
+            $(tick).hide()
+            target.append(tick)
+
+            $(tick).fadeIn(()=> {
+                $(item).slideUp()
+                $(item).fadeOut(() => {
+                    if (userNotes.children.length <= 2) {
+                        $(ghostCalss).fadeIn(() => {
+                            $(item).remove()
+                        })
+                    } else {
+                        $(item).remove()
+                    }
+                })
+            })
+        })
+    }
+})
+
+$('.items').on('click', '.copy', (e) => {
+    let content = e.currentTarget
+    .nextSibling
+    .parentNode
+    .children[1]
+    .children[0]
+    .innerText
+    navigator.clipboard.writeText(content)
+
+})
+
+$('.refresh').click( (e) => { 
+    RefreshNotes()
+})
+
+$( document ).ready(() => {
+    RefreshNotes()
+})
